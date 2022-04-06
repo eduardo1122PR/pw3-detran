44b1cad44cd31a0f69117fa1b9fc794174573557
Carlos <Matheus@gmail.com>
2022-04-06 14:08:43 -0300

    Criacao do projeto

---
+++ pw3-detran/pw3-detran/src/test/java/br/com/Carlos/Eduardo/pw3detran/detran/Pw3DetranApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Carlos.Eduardo.pw3detran.detran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3DetranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
